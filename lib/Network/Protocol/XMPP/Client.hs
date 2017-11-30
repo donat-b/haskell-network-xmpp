02460ff81dedc4f7be68ef6c43a63985968d6643
Alexander Vershilov <alexander.vershilov@gmail.com>
2017-11-30 21:26:11 +0300

    WIP

--- lib/Network/Protocol/XMPP/Client.hs
+++ lib/Network/Protocol/XMPP/Client.hs
@@ -1,17 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 -- Copyright (C) 2010-2011 John Millikin <jmillikin@gmail.com>
--- 
+--
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
 -- the Free Software Foundation, either version 3 of the License, or
 -- any later version.
--- 
+--
 -- This program is distributed in the hope that it will be useful,
 -- but WITHOUT ANY WARRANTY; without even the implied warranty of
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 -- GNU General Public License for more details.
--- 
+--
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -50,9 +50,9 @@ runClient server jid username password xmpp = do
 	rawHandle <- connectTo host port
 	IO.hSetBuffering rawHandle IO.NoBuffering
 	let handle = H.PlainHandle rawHandle
-	
+
 	-- Open the initial stream and authenticate
-	M.startXMPP handle "jabber:client" $ do
+  flip finally (H.hFlush handle) $ M.startXMPP handle "jabber:client" $ do
 		features <- newStream sjid
 		tryTLS sjid features $ \tlsFeatures -> do
 			let mechanisms = authenticationMechanisms tlsFeatures
@@ -87,7 +87,7 @@ authenticationMechanisms = step where
 -- | Send a @\<bind\>@ message for the given 'J.JID', returning the server's reply. In
 -- most cases the reply will be the same as the input. However, if the input has no
 -- 'J.Resource', the returned 'J.JID' will contain a generated 'J.Resource'.
--- 
+--
 -- Clients must bind a 'J.JID' before sending any 'Stanza's.
 bindJID :: J.JID -> M.XMPP J.JID
 bindJID jid = do
@@ -100,28 +100,28 @@ bindJID jid = do
 		>=> X.elementNodes
 		>=> X.isContent
 		>=> return . X.contentText
-	
+
 	let maybeJID = do
 		iq <- case bindResult of
 			ReceivedIQ x -> Just x
 			_ -> Nothing
 		payload <- iqPayload iq
-		
+
 		case getJID payload of
 			[] -> Nothing
 			(str:_) -> J.parseJID str
-	
+
 	returnedJID <- case maybeJID of
 		Just x -> return x
 		Nothing -> throwError (M.InvalidBindResult bindResult)
-	
+
 	-- Session
 	M.putStanza sessionStanza
 	void M.getStanza
-	
+
 	M.putStanza (emptyPresence PresenceAvailable)
 	void M.getStanza
-	
+
 	return returnedJID
 
 bindStanza :: Maybe J.Resource -> IQ
